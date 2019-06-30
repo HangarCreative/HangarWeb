2c4b0a3342fc4a6b4f0801ed595d41c84a7e501d
alen1433 <alen2herceg@gmail.com>
2019-06-30 10:57:34 +0200

    small changes to help the flow

--- Scripts/general.js
+++ Scripts/general.js
@@ -1,9 +1,11 @@
 window.onscroll = function() {
     var currentScrollPos = window.pageYOffset;
-    if (window.pageYOffset > 10) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-80px";
+    if(window.innerWidth < 768){
+        if (window.pageYOffset > 10) {
+            document.getElementById("navbar").style.top = "0";
+        } else {
+            document.getElementById("navbar").style.top = "-80px";
+        }
+        prevScrollpos = currentScrollPos;
     }
-    prevScrollpos = currentScrollPos;
 };
